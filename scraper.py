1b102653db064f6a9237d67471877ca75daf2bb1
jefrailey <jcaetof@gmail.com>
2014-06-17 07:06:42 -0700

    Add extract_listings() to scraper.py to parse out each listing on a CL apartment page.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import requests
 import os
+from bs4 import BeautifulSoup
+import sys
 
 
 def search_CL(bedrooms=None, minAsk=None, maxAsk=None, query=None):
@@ -25,3 +27,20 @@ with open('apartments.html', 'w') as outfile:
 def read_search_results(results='apartments.html'):
     with open(os.getcwd() + '/' + results, 'r') as source:
         return source.read(), 'utf-8'
+
+
+def parse_source(body, encoding='utf-8'):
+    return BeautifulSoup(body, from_encoding=encoding)
+
+
+def extract_listings(parsed_html):
+    listings = parsed_html.find_all('p', class_="row")
+    return listings
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        body, encoding = read_search_results()
+    else:
+        body, encoding = search_CL()
+    parsed = parse_source(body, encoding)
