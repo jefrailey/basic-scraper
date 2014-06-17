e5ef9ca9c089ce1da4ff363d0c5a5090785ae0c5
jefrailey <jcaetof@gmail.com>
2014-06-17 07:05:51 -0700

    Add test for extract listings that asserts each listing is a bs4.element.Tag

--- test_scraper.py
+++ test_scraper.py
@@ -1,9 +1,12 @@
 from scraper import search_CL
 from scraper import read_search_results
+from scraper import parse_source
+from scraper import extract_listings
+import bs4
 
 
 def test_search_CL():
-    test_body, test_encoding = search_CL(minAsk=100)
+    test_body, test_encoding = search_CL(minAsk=100, maxAsk=100)
     assert "<span class=\"desktop\">craigslist</span>" in test_body
     assert test_encoding == 'utf-8'
 
@@ -12,3 +15,17 @@ def test_read_search_result():
     test_body, test_encoding = read_search_results()
     assert "<span class=\"desktop\">craigslist</span>" in test_body
     assert test_encoding == 'utf-8'
+
+
+def test_parse_source():
+    test_body, test_encoding = read_search_results()
+    test_parse = parse_source(test_body, test_encoding)
+    assert isinstance(test_parse, bs4.BeautifulSoup)
+
+
+def test_extract_listings():
+    test_body, test_encoding = read_search_results()
+    test_parse = parse_source(test_body, test_encoding)
+    for row in extract_listings(test_parse):
+        print type(row)
+        assert isinstance(row, bs4.element.Tag)
