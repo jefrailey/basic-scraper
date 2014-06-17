5b3cdb3a1735a02fc10761b96f2d74b2fab8cd61
jefrailey <jcaetof@gmail.com>
2014-06-16 22:50:14 -0700

    Add test_read_search_result() to test reading data from a local .html

--- test_scraper.py
+++ test_scraper.py
@@ -1,7 +1,14 @@
 from scraper import search_CL
+from scraper import read_search_results
 
 
 def test_search_CL():
     test_body, test_encoding = search_CL(minAsk=100)
+    assert "<span class=\"desktop\">craigslist</span>" in test_body
+    assert test_encoding == 'utf-8'
+
+
+def test_read_search_result():
+    test_body, test_encoding = read_search_results()
     assert "<span class=\"desktop\">craigslist</span>" in test_body
     assert test_encoding == 'utf-8'
