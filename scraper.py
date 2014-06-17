90f43113ddb17f14bcc3828af9ee2ba393221c90
jefrailey <jcaetof@gmail.com>
2014-06-16 22:51:09 -0700

    Add read_search_results() to read a local .html so that a fresh GET isn't needed while experimenting with scraping.

--- scraper.py
+++ scraper.py
@@ -1,20 +1,27 @@
 import requests
+import os
 
 
 def search_CL(bedrooms=None, minAsk=None, maxAsk=None, query=None):
     url = "http://seattle.craigslist.org/search/apa"
     params = {}
     for k, v in locals().items():
-        params[k] = v
+        if v is not None:
+            params[k] = v
     if not params:
         raise ValueError("No keywords given")
     else:
         response = requests.get(url, params=params)
     if response.ok:
-        return response.text, response.encoding
+        return response.content, response.encoding
     else:
         response.raise_for_status()
 
-# output = search_CL()
-# with open('apartments.html', 'w') as outfile:
-#     outfile.write(output)
+response, encoding = search_CL()
+with open('apartments.html', 'w') as outfile:
+    outfile.write(response)
+
+
+def read_search_results(results='apartments.html'):
+    with open(os.getcwd() + '/' + results, 'r') as source:
+        return source.read(), 'utf-8'
